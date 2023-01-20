3b0cce4f2b960a02a2416828a1a45eaa14f59117
Ashwin Garg <108213911+AJ-1009@users.noreply.github.com>
2023-01-20 09:12:20 +0530

    Update Navbar.js

--- components/Navbar.js
+++ components/Navbar.js
@@ -4,7 +4,7 @@ import styles from './navbar.module.css'
 export default function Navbar() {
   return (
     <div className={styles['navbar']}>
-        <Link href="/apod" className={styles['link']} >NASA&#039;s Astronomy Picture of the Day</Link>
+        <Link href="/apod" className={styles['link']} >Astronomy Picture of the Day</Link>
         <Link href="/image" className={styles['link']} >Search space related images</Link>
         <Link href="/epic" className={styles['link']} >Earth Polychromatic Imaging Camera</Link>
     </div>
